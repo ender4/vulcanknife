1a5287eeb63840593e780c41c69494683bdf5dfa
Aron Ritchie <enderfour@gmail.com>
2012-05-30 21:15:14 -0700

    removed taxon index migration

--- db/migrate/20120401225446_add_index_to_spree_taxons_position.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexToSpreeTaxonsPosition < ActiveRecord::Migration
-  def self.up
-    add_index(:spree_taxons, :position)
-  end
-
-  def self.down
-    remove_index(:spree_taxons, :position)
-  end
-end
