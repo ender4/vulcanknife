073c43d0cf96e1b4a2029c26eb15c71623c39370
Aron Ritchie <enderfour@gmail.com>
2012-05-30 21:26:54 -0700

    removed some more migrations

--- db/migrate/20120530212159_add_availbilty_to_spree_products.rb
+++
@@ -1,8 +0,0 @@
-class AddAvailbiltyToSpreeProducts < ActiveRecord::Migration
-  def up
-    add_column :spree_products, :availability, :enum
-  end
-  def down
-    remove_column :spree_products, :availability
-  end
-end
