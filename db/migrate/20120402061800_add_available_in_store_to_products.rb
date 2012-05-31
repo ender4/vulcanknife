85a92e25831a96aaee31464b9ce9bb635fec4b13
Aron Ritchie <enderfour@gmail.com>
2012-05-30 21:21:27 -0700

    removed some more migrations

--- db/migrate/20120402061800_add_available_in_store_to_products.rb
+++
@@ -1,8 +0,0 @@
-class AddAvailableInStoreToProducts < ActiveRecord::Migration
-  def up
-    add_column :spree_products, :available_in_store_only, :boolean, :default => false
-  end
-  def down
-    remove_column :spree_products, :available_in_store_only
-  end
-end
--- db/migrate/20120530211210_remove_available_in_store_only_from_spree_products.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAvailableInStoreOnlyFromSpreeProducts < ActiveRecord::Migration
-  def up
-    remove_column :spree_products, :available_in_store_only
-  end
-  def down
-    add_column :spree_products, :available_in_store_only, :boolean, :default => false
-  end
-end
